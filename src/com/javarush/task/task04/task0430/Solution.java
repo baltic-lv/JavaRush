009cdbc08837cb401f5425a72445b817643e9d12
Juris Guba <liepajnieks@gmail.com>
2018-11-25 00:41:12 +0200

    Add a new solution

---
+++ src/com/javarush/task/task04/task0430/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task04.task0430;
+
+/*Вывести на экран числа от 1 до 10 используя цикл while. Каждое значение с новой строки.*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int x = 1;
+        while (x < 11) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
