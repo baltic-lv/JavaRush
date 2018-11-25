2022c4900e8420ba0d5ee854e525607095bd0cb7
Juris Guba <liepajnieks@gmail.com>
2018-11-25 23:38:33 +0200

    Add a new commit

---
+++ src/com/javarush/task/task05/Person.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task05;
+
+public class Person {
+    public String name;
+    public int age;
+    public String address;
+    public char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
