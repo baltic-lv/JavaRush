bee56c0db90da5848692368d4e5e375cac7a91b7
Juris Guba <liepajnieks@gmail.com>
2018-11-25 15:20:19 +0200

    Add a new commit

---
+++ src/com/javarush/task/task04/task0440/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task04.task0440;
+/*Используя цикл вывести на экран сто раз надпись:
+"Я никогда не буду работать за копейки. Амиго"
+Каждое значение вывести с новой строки.*/
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+        }
+    }
+}
