73dac292f0c23fe0f9feb5b2e3a1e4a78ade62e4
Juris Guba <liepajnieks@gmail.com>
2018-11-25 10:18:44 +0200

    Add a new solution

---
+++ src/com/javarush/task/task04/task0433/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0433;
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 0, j = 0;
+        String dollar = "";
+        while (i < 10) {
+            while (j < 10) {
+                dollar += "$";
+                j++;
+            }
+
+            System.out.print(dollar+"\n");
+            i++;
+        }
+
+    }
+}
