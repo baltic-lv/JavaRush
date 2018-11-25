b639c726b9e681bad212ba1cbb3b3e1fc55a84d7
Juris Guba <liepajnieks@gmail.com>
2018-11-25 22:47:14 +0200

    Add a new commit

---
+++ src/com/javarush/task/task05/task0503/Dog.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task05.task0503;
+/*Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+Создайте геттеры и сеттеры для всех переменных класса Dog.*/
+public class Dog {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int age;
+    public static void main(String[] args) {
+
+    }
+}
