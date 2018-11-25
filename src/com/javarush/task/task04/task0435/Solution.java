fa5090e62ef402f4bff4175a88c6a8f84ddbfbcb
Juris Guba <liepajnieks@gmail.com>
2018-11-25 13:50:29 +0200

    Add a new solution

---
+++ src/com/javarush/task/task04/task0435/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task04.task0435;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+}
