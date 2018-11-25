2b6b59441112f2cb29efd1b5d47977ec16f423b2
Juris Guba <liepajnieks@gmail.com>
2018-11-25 22:01:07 +0200

    Add a new commit

---
+++ src/com/javarush/task/task05/task0501/Cat.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task05.task0501;
+
+/*Создать класс Cat. У кота должно быть имя (name, String),
+ возраст (age, int), вес (weight, int), сила (strength, int).*/
+public class Cat {
+    public String name;
+    public int age;
+    public int weight;
+    public int strength;
+
+    public static void main(String[] args) {
+
+    }
+}
