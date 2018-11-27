4222b3298f93996b7d90e42cfaa0be5a4f2c1ee1
Juris Guba <liepajnieks@gmail.com>
2018-11-27 18:15:27 +0200

    Add a new commit

--- src/com/javarush/task/task05/task0514/Person.java
+++ src/com/javarush/task/task05/task0514/Person.java
@@ -4,7 +4,8 @@ package com.javarush.task.task05.task0514;
 Добавь метод initialize(String name, int age), в котором
 проинициализируй переменные name и age.
 В методе main создай объект Person, занеси его ссылку в переменную person.
-Вызови метод initialize с любыми значениями.*/
+Вызови метод initialize с любыми значениями.
+*/
 public class Person {
     public String name;
     public int age;
