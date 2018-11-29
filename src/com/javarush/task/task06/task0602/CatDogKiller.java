e8e446e01d1a71ee07842914798c3767d13bd978
Juris Guba <liepajnieks@gmail.com>
2018-11-29 22:03:01 +0200

    Add a new commit

--- src/com/javarush/task/task06/task0602/CatDogKiller.java
+++ src/com/javarush/task/task06/task0602/CatDogKiller.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task06.task0602;
-
+/*В каждом классе Cat и Dog написать метод finalize, который выводит на экран
+ текст о том, что такой-то объект уничтожен.*/
 public class CatDogKiller {
     public static void main(String[] args) {
 
