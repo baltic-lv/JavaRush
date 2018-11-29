d5ea35775ed49ff1360d392cafcb38a7d551edb5
Juris Guba <liepajnieks@gmail.com>
2018-11-29 14:44:32 +0200

    Add a new commit

---
+++ src/com/javarush/task/task05/task0527/TomAndJerry.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task05.task0527;
+
+public class TomAndJerry {
+    public static void main(String[] args) {
+        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+        Dog dog = new Dog("Tory",50,30);
+        Cat cat = new Cat("Tom",30,15);
+        //напишите тут ваш код
+    }
+
+    public static class Mouse {
+        String name;
+        int height;
+        int tail;
+
+        public Mouse(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+    public static class Cat {
+        String name;
+        int height;
+        int tail;
+
+        public Cat(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+    public static class Dog{
+        String name;
+        int height;
+        int tail;
+
+        public Dog(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+}
