048635fe1b4e60890531d3e746ed40bad87ea886
Juris Guba <liepajnieks@gmail.com>
2018-11-29 22:01:48 +0200

    Add a new commit

---
+++ src/com/javarush/task/task06/task0601/Cat.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task06.task0601;
+//В классе Cat создать метод protected void finalize() throws Throwable.
+public class Cat {
+    //напишите тут ваш код
+    protected void finalize() throws Throwable{
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
