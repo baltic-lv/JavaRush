dfd1145f3f9f5e00b638c9c149e02c1fa54219e1
Juris Guba <liepajnieks@gmail.com>
2018-12-01 19:55:38 +0200

    Add a new commit

---
+++ src/com/javarush/task/task06/task0615/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task06.task0615;
+
+/*Переставь один модификатор static, чтобы пример скомпилировался.*/
+public class Solution {
+
+    public static int A = 5;
+    public int B = 2;
+    public  int C = A * B;
+
+    public static void main(String[] args) {
+        A = 15;
+    }
+}
+
