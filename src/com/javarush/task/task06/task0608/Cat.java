73c6bfa81ed4d8f1b4b5aa9ba7b0f84ebfdd58f6
Juris Guba <liepajnieks@gmail.com>
2018-12-01 12:28:10 +0200

    Add a new commit

---
+++ src/com/javarush/task/task06/task0608/Cat.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task06.task0608;
+
+/*Добавить к классу Cat два статических метода:
+ int getCatCount() и setCatCount(int),
+с помощью которых можно получить/изменить количество котов (переменную catCount).
+
+*/
+public class Cat {
+    private static int catCount = 0;
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static int getCatCount() {
+        //напишите тут ваш код
+        return catCount;
+    }
+
+    public static void setCatCount(int catCount) {
+        //напишите тут ваш код
+        Cat.catCount = catCount;
+        // call to static variable via <ClassName>.<variable_name>
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
