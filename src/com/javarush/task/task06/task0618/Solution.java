c221694c5aeeba032464fadd5953036f9163d372
Juris Guba <liepajnieks@gmail.com>
2018-12-01 20:53:08 +0200

    Add a new commit

---
+++ src/com/javarush/task/task06/task0618/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task06.task0618;
+
+/*Создай класс с именем KissMyShinyMetalAss.
+Создай объект этого класса, выведи его на экран*/
+
+public class Solution {
+    public static class KissMyShinyMetalAss {
+
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        KissMyShinyMetalAss object = new KissMyShinyMetalAss();
+        System.out.println(object);
+    }
+}
+
+
